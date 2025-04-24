2b31b862e553a8e123495f646f499c9b970e642f
t.latush <t.latush@t-latush.local>
2025-04-24 15:14:45 +0200

    fqwfqw

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,4 +1,15 @@
-import {Image, ScrollView, Text, TouchableOpacity, Vibration, View, TextInput, Switch} from 'react-native';
+import {
+    Image,
+    ScrollView,
+    Text,
+    TouchableOpacity,
+    Vibration,
+    View,
+    TextInput,
+    Switch,
+    Alert,
+    Linking,
+} from 'react-native';
 import React, { useState, useEffect } from 'react';
 import RightYellowSvg from '../assets/svg/RightYellowSvg';
 import WhitePlusSvg from '../assets/svg/WhitePlusSvg';
@@ -18,42 +29,84 @@ const SettingsScreen = ({ navigation }) => {
 
                 </View>
             <View>
-                <TouchableOpacity style={{backgroundColor: '#002F1C', flexDirection: 'row', justifyContent: 'space-between', padding: 16, width: '100%', borderRadius: 16, alignItems: 'center', marginBottom: 8}}>
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: '#002F1C',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        padding: 16,
+                        width: '100%',
+                        borderRadius: 16,
+                        alignItems: 'center',
+                        marginBottom: 8,
+                    }}
+                    onPress={() => Alert.alert('Still in development')}
+                    activeOpacity={0.8}
+                >
                     <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
                         Dark theme
                     </Text>
-                    <Switch
-                        value={isDarkTheme}
-                        onValueChange={setIsDarkTheme}
-                    />
+                    {/*<Switch*/}
+                    {/*    value={isDarkTheme}*/}
+                    {/*    onValueChange={setIsDarkTheme}*/}
+                    {/*    onStartShouldSetResponder={() => true} // предотвращает срабатывание onPress*/}
+                    {/*/>*/}
                 </TouchableOpacity>
-                <TouchableOpacity style={{backgroundColor: '#002F1C', flexDirection: 'row', justifyContent: 'space-between', padding: 16, width: '100%', borderRadius: 16, alignItems: 'center', marginBottom: 8}}>
+
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: '#002F1C',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        padding: 16,
+                        width: '100%',
+                        borderRadius: 16,
+                        alignItems: 'center',
+                        marginBottom: 8,
+                    }}
+                    onPress={() => Alert.alert('Still in development')}
+                    activeOpacity={0.8}
+                >
                     <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
                         Notifications
                     </Text>
-                    <Switch
-                        value={isNotificationsEnabled}
-                        onValueChange={setIsNotificationsEnabled}
-                    />
+                    {/*<Switch*/}
+                    {/*    value={isNotificationsEnabled}*/}
+                    {/*    onValueChange={setIsNotificationsEnabled}*/}
+                    {/*    onStartShouldSetResponder={() => true}*/}
+                    {/*/>*/}
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.goBack()} style={{backgroundColor: '#002F1C',flexDirection: 'row', justifyContent: 'space-between', padding: 16, width: '100%', borderRadius: 16, alignItems: 'center', marginBottom: 8,}}>
-                    <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
+                <TouchableOpacity
+                    onPress={() => Linking.openURL('https://www.termsfeed.com/live/648aaa2f-ffa2-4a84-bc98-211fb04043ce')}
+                    style={{
+                        backgroundColor: '#002F1C',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        padding: 16,
+                        width: '100%',
+                        borderRadius: 16,
+                        alignItems: 'center',
+                        marginBottom: 8,
+                    }}
+                >                    <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
                         Privacy Policy
                     </Text>
                     <ChevronRight/>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.goBack()} style={{backgroundColor: '#002F1C',flexDirection: 'row', justifyContent: 'space-between', padding: 16, width: '100%', borderRadius: 16, alignItems: 'center', marginBottom: 8,}}>
+                <TouchableOpacity
+                    onPress={() => Linking.openURL('https://www.termsfeed.com/live/648aaa2f-ffa2-4a84-bc98-211fb04043ce')}
+                                                      style={{backgroundColor: '#002F1C',flexDirection: 'row', justifyContent: 'space-between', padding: 16, width: '100%', borderRadius: 16, alignItems: 'center', marginBottom: 8,}}>
                     <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
                         Terms of use
                     </Text>
                     <ChevronRight/>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.goBack()} style={{backgroundColor: '#002F1C',flexDirection: 'row', justifyContent: 'space-between', padding: 16, width: '100%', borderRadius: 16, alignItems: 'center', marginBottom: 8,}}>
-                    <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
-                        About developer
-                    </Text>
-                    <ChevronRight/>
-                </TouchableOpacity>
+                {/*<TouchableOpacity onPress={() => navigation.goBack()} style={{backgroundColor: '#002F1C',flexDirection: 'row', justifyContent: 'space-between', padding: 16, width: '100%', borderRadius: 16, alignItems: 'center', marginBottom: 8,}}>*/}
+                {/*    <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>*/}
+                {/*        About developer*/}
+                {/*    </Text>*/}
+                {/*    <ChevronRight/>*/}
+                {/*</TouchableOpacity>*/}
             </View>
         </View>
     );
